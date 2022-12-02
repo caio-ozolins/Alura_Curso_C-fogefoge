6ff0c02f54bf9dbd4950e17f11df596301b42f6f
Caio Ozolins Mendes <caio.mendes@alunos.unis.edu.br>
2022-12-02 15:54:41 -0300

    refactor(encontramapa): change function from void to int
    Change function encontramapa() to return a boolean
    
    Closes: 03.10 - Implementando a finalização do jogo

--- mapa.c
+++ mapa.c
@@ -37,16 +37,17 @@ void imprimemapa(MAPA* m){
         printf("%s\n", m->matriz[i]);
     }
 }
-void encontramapa(MAPA* m, POSICAO* p, char c){
+int encontramapa(MAPA* m, POSICAO* p, char c){
     for (int i = 0; i < m->linhas; ++i) {
         for (int j = 0; j < m->colunas; ++j) {
             if (m->matriz[i][j] == c){
                 p->x = i;
                 p->y = j;
-                break;
+                return 1;
             }
         }
     }
+    return 0;
 }
 int ehvalida(MAPA* m, int x, int y){
     if (x >= m->linhas)
